e42592f07a123115361113916e8f344273a11648
jun ma <majun@huobi.com>
2018-11-21 01:33:45 +0000

    Add killing process. Ready for cronjob.

--- r.sh
+++ r.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+ps aux | grep bitcoinexchangefh | awk '{print $2}' | xargs kill -KILL
 cd /home/ec2-user/junma/BitcoinExchangeFH;nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts selected.ini &
 
