4433bdc250344fce48c6645da661039ec94c4b7f
jun ma <majun@huobi.com>
2018-11-22 02:01:31 +0000

    .

--- r.sh
+++ r.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ps aux | grep bitcoinexchangefh | awk '{print $2}' | xargs kill -KILL
+sleep 0.1
 cd /home/ec2-user/junma/BitcoinExchangeFH;nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts selected.ini &
 
