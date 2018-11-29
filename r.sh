344aa4cd792cc2720cb686665cf4cc3443d7c1c6
jun ma <majun@huobi.com>
2018-11-29 08:41:57 +0000

    .

--- r.sh
+++ r.sh
@@ -6,6 +6,5 @@ cd /home/ec2-user/junma/BitcoinExchangeFH
 nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts selected.ini &
 nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts hbdm.ini &
 nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts gdax.ini &
-nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts bitmex_index.ini &
 
  
