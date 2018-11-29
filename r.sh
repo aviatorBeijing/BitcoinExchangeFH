e80a64cecf39699aac6b6f7ba7ecf874ca2fdada
jun ma <majun@huobi.com>
2018-11-29 08:31:01 +0000

    .

--- r.sh
+++ r.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ps aux | grep bitcoinexchangefh | awk '{print $2}' | xargs kill -KILL
-sleep 0.1
-cd /home/ec2-user/junma/BitcoinExchangeFH;nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts selected.ini &
-
+sleep 1
+cd /home/ec2-user/junma/BitcoinExchangeFH
+nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts selected.ini &
+nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts hbdm.ini &
+nohup bitcoinexchangefh -mysql -mysqldest "bitcoin:bitcoin@172.25.1.210:3306" -mysqlschema bcex -instmts gdax.ini &
